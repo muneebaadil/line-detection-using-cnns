c81d4624a4d717a0895f4b880a6ec25ddd7e415b
Muneeb Aadil <imuneebaadil@gmail.com>
2017-11-04 20:41:59 +0500

    added skeleton train.py and dataloader.py

---
+++ src/train.py
@@ -0,0 +1,17 @@
+import argparse
+import numpy as np
+
+def train(opts): 
+	""""""
+	pass
+
+def set_arguments(parser): 
+	pass
+
+if __name__=='__main__': 
+	parser = argparse.ArgumentParser()
+	set_arguments(parser)
+
+	opts = parser.parse_args()
+
+	train(opts)
