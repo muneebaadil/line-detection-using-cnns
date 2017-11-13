d81c28c3d91ed7233b4914d551da53d7bf1bcf2b
Muneeb Aadil <imuneebaadil@gmail.com>
2017-11-13 17:00:19 +0500

    Added binary cross entropy in losses.py

--- src/losses.py
+++ src/losses.py
@@ -1,4 +1,6 @@
 #THIS FILE CONTAINS ALL THE LOSS FUNCTIONS WE EXPERIMENTED WITH.. 
 
+import keras.metrics as mts 
+
 losses_dict = dict()
-#to do: add binary cross entropy here..
+losses_dict['binary_crossentropy'] = mts.binary_crossentropy
