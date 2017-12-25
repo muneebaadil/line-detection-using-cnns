d7840eb52941038c1c8a6849729c22ba60743601
Muneeb Aadil <imuneebaadil@gmail.com>
2017-12-25 18:40:07 +0500

    changed model_init to contain sequential api

--- src/models.py
+++ src/models.py
@@ -2,21 +2,39 @@
 
 import keras.layers as layers 
 from keras.initializers import VarianceScaling
-from keras.models import Model
+from keras.models import Model, Sequential
 
 def model_init(opts):
     """Simply two layer convolutional neural network for initial testing purposes"""
     
     init_fn = VarianceScaling(2.)
-    inputs = layers.Input(shape=(256,256,3))
+    # inputs = layers.Input(shape=(256,256,3))
     
-    c1 = layers.Conv2D(32,kernel_size=3,strides=1,padding='valid',activation='relu',
-                            kernel_initializer=init_fn)(inputs)
-    c2 = layers.Conv2D(32, kernel_size=3, strides=1, padding='valid', activation='relu',
-                        kernel_initializer=init_fn)(c1)
+    # c1 = layers.Conv2D(32,kernel_size=3,strides=1,padding='valid',activation='relu',
+    #                         kernel_initializer=init_fn)(inputs)
+    # if opts.dropout >= 0: 
+        
+    # c2 = layers.Conv2D(32, kernel_size=3, strides=1, padding='valid', activation='relu',
+    #                     kernel_initializer=init_fn)(c1)
 
-    model = Model(inputs=inputs,outputs=c2)
-    return model
+    # model = Model(inputs=inputs,outputs=c2)
+    # return model
+
+    model = Sequential()
+
+    model.add(layers.Conv2D(32,kernel_size=3,strides=1,padding='same',activation='relu',
+                             kernel_initializer=init_fn, input_shape=(512,512,1)))
+    if opts.dropRate >= 0: 
+        model.add(layers.Dropout(rate=opts.dropRate))
+    
+    model.add(layers.Conv2D(32,kernel_size=3,strides=1,padding='same',activation='relu',
+                             kernel_initializer=init_fn))
+    if opts.dropRate >= 0: 
+        model.add(layers.Dropout(opts.dropRate))
+
+    model.add(layers.Conv2D(1,kernel_size=1,strides=1,padding='same',activation='relu',
+                            kernel_initializer=init_fn))
+    
 
 models_dict = dict()
 models_dict['model_init'] = model_init
