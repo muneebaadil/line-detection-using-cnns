3825baa17723b60808eebef20e497e5a0b1fcf84
Muneeb Aadil <imuneebaadil@gmail.com>
2017-12-28 12:43:58 +0500

    removed yet another bug from weights of objective functions

--- src/losses.py
+++ src/losses.py
@@ -7,7 +7,8 @@ def BinaryCrossEntropy(y_true, y_pred):
 def WeightedBinaryCrossEntropy(y_true, y_pred):
 	err = -((y_true*K.log(y_pred)) + ((1-y_true)*K.log(1-y_pred)))
 	probs = K.mean(y_true,axis=(1,2,3),keepdims=True)
-	weights = 1./probs
+	weights_pos, weights_neg = 1./probs, 1./(1-probs)
+	weights = (y_true*weights_pos) + ((1-y_true)*weights_neg)
 	return K.mean(err*weights)
 
 def L2Loss(y_true,y_pred): 
@@ -16,7 +17,8 @@ def L2Loss(y_true,y_pred):
 def WeightedL2Loss(y_true,y_pred): 
 	err = K.square(y_true-y_pred)
 	probs = K.mean(y_true,axis=(1,2,3),keepdims=True)
-	weights = 1./probs
+	weights_pos, weights_neg = 1./probs, 1./(1-probs)
+	weights = (y_true*weights_pos) + ((1-y_true)*weights_neg)
 	return K.mean(weights*err)
 
 losses_dict={'binaryCrossEntropy':BinaryCrossEntropy,'weightedBinaryCrossEntropy':WeightedBinaryCrossEntropy, 
