25e87d23e554825727b9d43b6ad756e37a79b021
Muneeb Aadil <imuneebaadil@gmail.com>
2017-12-28 21:06:30 +0500

    adopted factory function for input weightage

--- src/losses.py
+++ src/losses.py
@@ -4,12 +4,17 @@ def BinaryCrossEntropy(y_true, y_pred):
 	losses = -((y_true*K.log(y_pred)) + ((1-y_true)*K.log(1-y_pred)))
 	return K.mean(losses)
 
-def WeightedBinaryCrossEntropy(y_true, y_pred):
-	err = -((y_true*K.log(y_pred)) + ((1-y_true)*K.log(1-y_pred)))
-	probs = K.mean(y_true,axis=(1,2,3),keepdims=True)
-	weights_pos, weights_neg = 1./probs, 1./(1-probs)
-	weights = (y_true*weights_pos) + ((1-y_true)*weights_neg)
-	return K.mean(err*weights)
+def WeightedBinaryCrossEntropy(x_true): 
+	def WeightedBinaryCrossEntropy_(y_true, y_pred):
+		err = -((y_true*K.log(y_pred)) + ((1-y_true)*K.log(1-y_pred)))
+
+		probs = K.mean(x_true,axis=(1,2,3),keepdims=True)
+		weights_pos, weights_neg = 1./probs, 1./(1-probs)
+		weights = (x_true*weights_pos) + ((1-x_true)*weights_neg)
+
+		return K.mean(err*weights)
+
+	return WeightedBinaryCrossEntropy_
 
 def L2Loss(y_true,y_pred): 
 	return K.mean(K.square(y_true-y_pred))
