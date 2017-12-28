83c89bc7f2360a92f7b948e3cb47d5d1eea0d7ea
Muneeb Aadil <imuneebaadil@gmail.com>
2017-12-28 12:33:41 +0500

    added l2 losses variations

--- src/losses.py
+++ src/losses.py
@@ -10,4 +10,14 @@ def WeightedBinaryCrossEntropy(y_true, y_pred):
 	weights = 1./probs
 	return K.mean(err*weights)
 
-losses_dict={'binaryCrossEntropy':BinaryCrossEntropy,'weightedBinaryCrossEntropy':WeightedBinaryCrossEntropy}
+def L2Loss(y_true,y_pred): 
+	return K.mean(K.square(y_true-y_pred))
+
+def WeightedL2Loss(y_true,y_pred): 
+	err = K.square(y_true-y_pred)
+	probs = K.mean(y_true,axis=(1,2,3),keepdims=True)
+	weights = 1./probs
+	return K.mean(weights*err)
+
+losses_dict={'binaryCrossEntropy':BinaryCrossEntropy,'weightedBinaryCrossEntropy':WeightedBinaryCrossEntropy, 
+			'l2Loss': L2Loss, 'weightedL2Loss':WeightedL2Loss}
