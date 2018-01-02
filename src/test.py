adc2302d2a694a92234ee6d696a1ab66d48a99bc
Muneeb Aadil <imuneebaadil@gmail.com>
2018-01-02 14:47:09 +0500

    removed evaluation part in test.py

--- src/test.py
+++ src/test.py
@@ -31,38 +31,13 @@ def Test(opts):
 
     model = load_model(opts.modelPath,compile=False)
 
+    #predicting and saving predicted images..
     predY = model.predict(imgsX, batch_size=opts.batchSize, verbose=opts.verbosity)
 
-    p,r,f,_ = precision_recall_fscore_support(y_true=imgsY.flatten(), y_pred=predY.flatten() > opts.decideThreshold,
-                                    labels=[False,True])
+    CheckAndCreate(opts.outDir)
 
-    print '[decideThreshold = {}] precision={}, recall={}, fscore(beta=1.)={}'.format(opts.decideThreshold,p,r,f,_)
-    print 'precision_avg={}, recall_avg={}, fcore_avg(beta=1.)={}'.format(p.mean(),r.mean(),f.mean())
-
-    if opts.saveTestOutput==True: 
-        CheckAndCreate(opts.outDir)
-
-        for predY_,fnameX in izip(predY,fnamesX): 
-            io.imsave(os.path.join(opts.outDir,fnameX), predY_[:,:,0])
-
-    if opts.saveAnalysis==True: 
-        CheckAndCreate(opts.outDir)
-
-        for ypred, yout in izip(predY,imgsY): 
-            analysis = np.zeros((yout.shape[0],yout.shape[1],3))
-
-            ypred=ypred[:,:,0]
-            yout=yout[:,:,0]
-
-            p,r,f,_=precision_recall_fscore_support(yout.flatten(),ypred.flatten()>opts.decideThreshold,
-                                                    labels=[False,True])
-            
-            analysis[(ypred>.5)==False] = np.array([0.,0.,0.])
-            analysis[(ypred>.5)==True] = np.array([1.,1.,1.])
-            analysis[np.logical_and((ypred>.5)==True, yout==False)] = np.array([1.,0.,0.])
-            analysis[np.logical_and((ypred>.5)==False, yout==True)] = np.array([0.,0.,1.])
-
-            io.imsave(os.path.join(opts.outDir,'analysis_'+fnameX), analysis)
+    for predY_,fnameX in izip(predY,fnamesX): 
+        io.imsave(os.path.join(opts.outDir,fnameX), predY_[:,:,0])
     return 
 
 def SetArguments(parser): 
@@ -77,17 +52,14 @@ def SetArguments(parser):
     parser.add_argument('-modelExpName', action='store',type=str, default='strftime("%d-%m-%Y__%H-%M-%S",gmtime())', dest='modelExpName')
 
     #Other parameters 
-    parser.add_argument('-decideThreshold', action='store',type=float, default=.5, dest='decideThreshold')
     parser.add_argument('-batchSize', action='store',type=int, default=1, dest='batchSize')
     parser.add_argument('-verbosity', action='store',type=int, default=1, dest='verbosity')
-    parser.add_argument('-saveTestOutput', action='store',type=bool, default=False, dest='saveTestOutput')
-    parser.add_argument('-saveAnalysis', action='store',type=bool, default=False, dest='saveAnalysis')
     return 
 
 def PostprocessOpts(opts): 
     opts.outDir = os.path.join(opts.outRootDir, opts.modelExpName)
     opts.modelPath = os.path.join(opts.expRootDir, opts.modelExpName)
-    opts.modelPath = os.path.join(opts.modelPath, 'model', os.listdir(os.path.join(opts.modelPath, 'model'))[0])
+    opts.modelPath = os.path.join(opts.modelPath, 'model', sorted(os.listdir(os.path.join(opts.modelPath, 'model')))[-1])
     return
 
 if __name__=='__main__':
